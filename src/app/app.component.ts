b9fa41dc95fd5e080e82dcb0fce61dce6f0349da
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-22 18:04:45 +0530

    added new template for product list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+//import { template } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
+
+  // template: "<h1>{{pageTitle}}</h1>" //inline template
+  // template:                          //inline template back ticks
+  // `<div>
+  //   <h1> {{pageTitle}} </h1>
+  // </div>` 
 })
 
 export class AppComponent {
