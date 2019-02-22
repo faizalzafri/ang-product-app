3ed82225aee4b37e102b0d894fb1c68fa9af2b73
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-22 18:23:31 +0530

    using selectors and component as directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule,
     AppRoutingModule],
   providers: [],
