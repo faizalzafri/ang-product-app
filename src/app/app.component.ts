e7c2f4adc141eb84efe25752fe7889c56861c2d3
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-22 16:09:27 +0530

    refactored module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { template } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
-  template: '<h1> {{pageTitle}} </h1>'
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   pageTitle: String = 'ACME Product Management';
 }
