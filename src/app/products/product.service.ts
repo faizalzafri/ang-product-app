5a52d0de5c05decc59ad6d04ab4ecf680e5161a1
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-23 19:06:30 +0530

    added exception handling for get request

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -10,10 +11,29 @@ export class ProductService {
 
     private productUrl = 'api/products/product.json';
 
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient) {
     }
 
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl);
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(jsonData => console.log(jsonData)),
+            catchError(this.handleError)
+        );
+    }
+
+    handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(err.error.message);
     }
 }
