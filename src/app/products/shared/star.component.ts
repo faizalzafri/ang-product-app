81485cafeb3208c717a499bfe1992f7a12841400
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-23 11:37:14 +0530

    responding to click event on star compoent

--- src/app/products/shared/star.component.ts
+++ src/app/products/shared/star.component.ts
@@ -9,10 +9,15 @@ export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
 
+
     // using onchanges to capture the changes made to property
     //onchanges watches changes to input properties
     ngOnChanges(): void {
         //starwidth is supposed to change on click
         this.starWidth = this.rating * 75 / 5;
+    }
+
+    onClick():void{
+        
     }
 }
