57713f6c2e74facaa367d6add58c2a37682606f2
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-23 18:26:13 +0530

    using service to get product data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,13 +6,15 @@ import { ProductService } from './product.service';
     selector: 'app-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css'],
-    providers: [ProductService]
+    //providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = "Product List";
     imageWidth: number = 40;
     imageMargin: number = 2;
     showImage: boolean = false;
+
+    private _productService;
 
     _listFilter: string;
     get listFilter(): string {
@@ -25,28 +27,7 @@ export class ProductListComponent implements OnInit {
 
     //any is for cases where data type is not specified
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
+    products: IProduct[];
 
     toggleImage(): void {
         this.showImage = !this.showImage;
@@ -54,11 +35,13 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('Inside OnInit');
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+        console.log('Inside Constructor');
+        this._productService = productService;
     }
 
     performFilter(filterBy: string): IProduct[] {
